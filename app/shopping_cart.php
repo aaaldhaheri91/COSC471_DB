d4a3605fca2a979ce95f5ea2da7230c11e0f8189
aaaldhaheri91 <aaaldhaheri91@gmail.com>
2017-11-20 21:04:15 -0500

    shopping_cart fully functional

--- app/shopping_cart.php
+++ app/shopping_cart.php
@@ -6,17 +6,53 @@
 		error_reporting(E_ALL);
 		ini_set("display_errors","On");
 		session_start();
-		$filepath = realpath(dirname(__FILE__));
-		require_once($filepath .'/db_session.php');
 
+		$data = array();
+		$i = 0;
+		$shopping_cart_total = 0;
 
+		//delete book from shopping cart if delete button selected
+		if(isset($_GET['delIsbn'])){
+			foreach($_SESSION as $data){
+				if($data['isbn'] == $_GET['delIsbn']){
+					unset($_SESSION[$data['title']]);
+				}
+			}
+		}
 
 	?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script>
+
+	$(document).ready(function(){
+		calculate_payment();
+	});
+
 	//remove from cart
 	function del(isbn){
 		window.location.href="shopping_cart.php?delIsbn="+ isbn;
 	}
+
+	function calculate_payment(){
+		var total = 0;
+		var i = 0;
+
+		for(;;){
+			if($('#quantity' + i).length == 0){
+				break;
+			}
+			else {
+				quantity = $('#quantity' + i).val();
+				price = $('#price' + i).text();
+				price = price.replace('$', '');
+				total += Number(quantity) * Number(price);
+			}
+			i++
+		}
+		total = total.toFixed(2);
+		$('#total').html('$' + total);
+	}
+
 	</script>
 </head>
 <body>
@@ -39,25 +75,39 @@
 			</td>
 		</tr>
 		<tr>
-				<form id="recalculate" name="recalculate" action="" method="post">
+				<!-- <form id="recalculate" name="recalculate" action="" method="post"> -->
 			<td  colspan="3">
 				<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;">
 					<table align="center" BORDER="2" CELLPADDING="2" CELLSPACING="2" WIDTH="100%">
 						<th width='10%'>Remove</th><th width='60%'>Book Description</th><th width='10%'>Qty</th><th width='10%'>Price</th>
-											</table>
+						<?php
+							$i = 0;
+							foreach ($_SESSION as $data) {
+								if(isset($data['title'])){
+									echo "<tr><td><button name='delete' onClick='del(" . $data['isbn'] . ")'>Delete Item </button></td>
+											  <td><p>" . $data['title'] . "<br><strong>By: </strong>" . $data['Fname'] . ' ' . $data['Lname'] . "</p></td>
+												<td><input id='quantity" . $i . "' type='text' style='width: 16px; height: 16px;' value='1'></td><td id='price" . $i . "'>" . $data['price'] . "</td></tr>";
+									$i++;
+								}
+
+							}
+
+						 ?>
+					</table>
 				</div>
 			</td>
 		</tr>
 		<tr>
 			<td align="center">
-					<input type="submit" name="recalculate_payment" id="recalculate_payment" value="Recalculate Payment">
-				</form>
+					<button name="recalculate_payment" onClick="calculate_payment()" id="payment">Recalculate Payment</button>
+				<!-- </form> -->
 			</td>
 			<td align="center">
 				&nbsp;
 			</td>
 			<td align="center">
-				Subtotal:  $0			</td>
+				Subtotal: <span id='total'>$0</span>
+			</td>
 		</tr>
 	</table>
 </body>
