94b9c39959537cc4c056c8c77d87ff502a9e8c0b
aaaldhaheri91 <aaaldhaheri91@gmail.com>
2017-12-10 20:02:28 -0500

    adding Jon changes

--- app/admin_login.php
+++ app/admin_login.php
@@ -5,8 +5,34 @@
 </head>
 
 <body>
+	<?php 
+		$name = $_POST['adminname'];
+		$pin = $_POST['pin'];
+		
+		session_start();
+		$filepath = realpath(dirname(__FILE__));
+		require_once($filepath .'/db_session.php');
+
+		$data = array();
+		$db_session = new DB_Session();
+		$database = $db_session->OpenCon();
+		
+		$data = $database->query("SELECT * FROM users WHERE isAdmin = 1");
+		
+		if (!is_null($_POST[adminname])) {
+			if($data) {
+			$row = $data->fetch_assoc();
+			if($name == $row['username'] && $pin == $row['pin']) {
+				header("Location: http://db2.emich.edu/~201709_cosc471_group02/COSC471_DB/app/reports.php");
+			}
+			else {
+				echo '<script type="text/javascript">alert("Please Enter Valid Admin Credentials!");</script>';
+			}
+		}
+		}
+	?>
 <table align="center" style="border:2px solid blue;">
-		<form action="reports.php" method="post" id="adminlogin_screen">
+		<form action="admin_login.php" method="post" id="adminlogin_screen">
 		<tr>
 			<td align="right">
 				Adminname<span style="color:red">*</span>:
--- app/reports.php
+++ app/reports.php
@@ -1,10 +1,113 @@
-
-</br></br></br><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Customer Report</td></tr><tr><td align='center'>Total number of customers till date</td><td align='center'>64</td></tr></table></br></br></br><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Stock report</td></tr><tr><td align='center'>Fantasy</td><td align='center'>-15</td></tr><tr><td align='center'>Adventure</td><td align='center'>11</td></tr></table></br></br></br><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>27.625000000000004</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>13.007407407407406</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>50.5</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>18.7</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>6.7727272727272725</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>7.45</td></tr></table><table align='center' border='2' style='border:2px solid blue;'><tr><td align='center' colspan='2'>Sales Report</td></tr><tr><td align='center'>Sales average till date</td><td align='center'>47.3625</td></tr></table><form action='screen1.php' method='post' align='center'><input type='submit' name='cancel' value='EXIT 3-B.com[Admin]'></form><html>
+<html>
 <head>
-<title>Reports</title>
+	<title>Reports</title>
+	<style>
+	</style>
 </head>
 <body>
+	<?php		
+		session_start();
+		$filepath = realpath(dirname(__FILE__));
+		require_once($filepath .'/db_session.php');
+
+		$db_session = new DB_Session();
+		$database = $db_session->OpenCon();
+		
+		$data = $database->query("SELECT count(*) FROM users");
+		$row = $data->fetch_assoc();
+		$num_users = $row['count(*)'];
+/*		
+		$data = $database->query("SELECT category, count(category) FROM `books` GROUP BY category")
+					
+			echo "<table><tr><td>Stock Report</td></tr>";
+			
+			while($row = $data->fetch_assoc()) {
+						echo "<tr><td>" . $row['category'] . "</td><td>" . $row['count(category)'] . "</td></tr>";
+			}
+			echo "</table>";
+*/
+	?>		
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Customer Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Total number of customers till date</td>
+			<td align='center'><?php echo $num_users;?></td>
+		</tr>
+	</table>
 	
+	<br>
+	<br>
+	<br>
+	
+	
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>27.625000000000004</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>13.007407407407406</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>50.5</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>18.7</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>6.7727272727272725</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>7.45</td>
+		</tr>
+	</table>
+	<table align='center' border='2' style='border:2px solid blue;'>
+		<tr>
+			<td align='center' colspan='2'>Sales Report</td>
+		</tr>
+		<tr>
+			<td align='center'>Sales average till date</td>
+			<td align='center'>47.3625</td>
+		</tr>
+	</table>
+	<form action='screen1.php' method='post' align='center'>
+		<input type='submit' name='cancel' value='EXIT 3-B.com[Admin]'>
+	</form>
 </body>
 </html>
 
